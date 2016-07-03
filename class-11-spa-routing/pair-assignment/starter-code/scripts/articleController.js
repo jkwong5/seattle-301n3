3021758710ade73eeb6dbe30c7bbd491c0363cc6
Jonathan Wong <jkwong5@gmail.com>
2016-07-03 11:13:00 -0700

    completed pair assignment with Lillian

--- class-11-spa-routing/pair-assignment/starter-code/scripts/articleController.js
+++ class-11-spa-routing/pair-assignment/starter-code/scripts/articleController.js
@@ -1,14 +1,14 @@
 (function(module) {
   var articlesController = {};
-
-  // TODO: Create the `articles` table when the controller first loads, with the code that used to be in index.html:
-
-  // TODO: Setup a function that kicks off the fetching and rendering of articles, using the same
+  // DONE: Create the `articles` table when the controller first loads, with the code that used to be in index.html:
+  Article.createTable();
+  // DONE: Setup a function that kicks off the fetching and rendering of articles, using the same
   // code that used to be in index.html.
-  // TODO: Also be sure to hide all the main section elements, and reveal the #articles section:
+  Article.fetchAll(articleView.initIndexPage);
+  // DONE: Also be sure to hide all the main section elements, and reveal the #articles section:
   articlesController.index = function() {
-
+    $('.tab-content').hide();
+    $('#articles').show();
   };
-
   module.articlesController = articlesController;
 })(window);
