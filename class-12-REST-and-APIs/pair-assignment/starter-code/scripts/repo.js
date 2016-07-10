e9a46d176af132e5bfde43e8a486e924a097d29d
Jonathan Wong <jkwong5@gmail.com>
2016-07-10 09:56:45 -0700

    changed api url

--- class-12-REST-and-APIs/pair-assignment/starter-code/scripts/repo.js
+++ class-12-REST-and-APIs/pair-assignment/starter-code/scripts/repo.js
@@ -6,9 +6,8 @@
   repos.requestRepos = function(callback) {
     // TODO: How would you like to fetch your repos? Don't forget to call the callback.
 
-<<<<<<< HEAD
     $.ajax({
-      url: 'https://api.github.com/users/jkwong5/repos?per_page=10&sorted=updated',
+      url: 'https://api.github.com/user/repos?per_page=10&sorted=updated',
       type: 'GET',
       headers: {'authorization': 'token ' + token},
       success: function(data){
@@ -18,8 +17,6 @@
         // });
       }
     }).done(callback);
-=======
->>>>>>> ef2f25eee6ec31e4df18aa5e7a626c2e31cbb700
   };
 
   // DONE: Model method that filters the full collection for repos with a particular attribute.
